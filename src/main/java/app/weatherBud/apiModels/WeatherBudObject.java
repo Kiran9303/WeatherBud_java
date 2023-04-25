31b961c05a381695fda05417c3ed1f8094c11b1a
chandra kiran addagarla <chandrak9303@gmail.com>
2023-04-24 23:04:36 -0400

    updating the API with query params and using constants

--- src/main/java/app/weatherBud/apiModels/WeatherBudObject.java
+++ src/main/java/app/weatherBud/apiModels/WeatherBudObject.java
@@ -1,7 +1,9 @@
 package app.weatherBud.apiModels;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
+@ApiModel(value = "response object info")
 @Data
 public class WeatherBudObject {
     public WeatherBudLocationModel location;
